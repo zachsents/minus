73eb5c229fb61bb9174393fbf02702b6a427d930
Zach Sents <zachsents@gmail.com>
2023-08-08 22:26:03 -0600

    Added storage to terraform

--- main.tf
+++ main.tf
@@ -108,3 +108,62 @@ resource "google_firestore_database" "database" {
 
   depends_on = [google_project_service.firestore]
 }
+
+# Enable Storage
+resource "google_project_service" "storage" {
+  project    = google_project.default.project_id
+  service    = "firebasestorage.googleapis.com"
+  depends_on = [google_firebase_project.default]
+}
+
+# Create default Cloud Storage bucket via Google App Engine
+resource "google_app_engine_application" "default" {
+  provider = google-beta
+  project  = google_project.default.project_id
+  # See available locations: https://firebase.google.com/docs/projects/locations#default-cloud-location
+  # This will set the location for the default Storage bucket and the App Engine App.
+  location_id = "us-central"
+
+  # If you use Firestore, uncomment this to make sure Firestore is provisioned first.
+  depends_on = [
+    google_firestore_database.database
+  ]
+}
+
+# Makes the default Storage bucket accessible for Firebase SDKs, authentication, and Firebase Security Rules.
+resource "google_firebase_storage_bucket" "default-bucket" {
+  provider  = google-beta
+  project   = google_project.default.project_id
+  bucket_id = google_app_engine_application.default.default_bucket
+
+  depends_on = [
+    google_project_service.storage,
+  ]
+}
+
+# Creates a ruleset of Cloud Storage Security Rules from a local file.
+resource "google_firebaserules_ruleset" "storage" {
+  provider = google-beta
+  project  = google_project.default.project_id
+  source {
+    files {
+      # Write security rules in a local file named "storage.rules".
+      # Learn more: https://firebase.google.com/docs/storage/security/get-started
+      name    = "storage.rules"
+      content = file("storage.rules")
+    }
+  }
+
+  # Wait for the default Storage bucket to be provisioned before creating this ruleset.
+  depends_on = [
+    google_firebase_project.default,
+  ]
+}
+
+# Releases the ruleset to the default Storage bucket.
+resource "google_firebaserules_release" "default-bucket" {
+  provider     = google-beta
+  name         = "firebase.storage/${google_app_engine_application.default.default_bucket}"
+  ruleset_name = "projects/${google_project.default.project_id}/rulesets/${google_firebaserules_ruleset.storage.name}"
+  project      = google_project.default.project_id
+}
--- variables.tf
+++ variables.tf
@@ -1,4 +1,8 @@
 
-variable "gcloud_project" {}
+variable "gcloud_project" {
+  default = "minus-393905"
+}
 
-variable "gcloud_billing_account" {}
+variable "gcloud_billing_account" {
+  default = "01C6B3-DB562A-403425"
+}
